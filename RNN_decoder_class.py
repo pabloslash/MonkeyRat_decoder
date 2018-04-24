af16f0e6fb0fcca9dab5585390a376e68305af2c
Pablo <pablo.tostado@yahoo.es>
2018-04-23 21:32:07 -0700

    Finished version of working class.

--- RNN_decoder_class.py
+++ RNN_decoder_class.py
@@ -12,12 +12,18 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, LSTM, SimpleRNN, GRU
 from keras.regularizers import l2, l1 #activity_l2, l1
 from keras.callbacks import EarlyStopping
+import IPython as IP
 
 
 class Rat_decoder(object):
-    def __init__(self, data_dir):
+    def __init__(self, data_dir, bins_before, units, dropout, num_epochs):
         self.data_dir = data_dir
+        self.save_dir = self.data_dir + 'PredResuls/'
         self.val_data = None
+        self.bins_before = bins_before
+        self.units = units
+        self.dropout = dropout
+        self.num_epochs = num_epochs
 
     #This function gets the VAF
     def get_corr(self, y_test, y_test_pred):
@@ -30,10 +36,12 @@ class Rat_decoder(object):
     # Might give errors in the future if not in the same format
     def get_EMG_name(self, signal):
         EMGname = ''
-        if 'EMG' in self.decoding_sig:
+        if (self.sigs_labels.size > 0): #If we specified signal labels
             EMGname = self.sigs_labels[0,signal][0].encode('utf8')
+        elif 'EMG' in self.decoding_sig:
+            EMGname = 'EMG' + str(signal)
         elif 'KIN' in self.decoding_sig:
-            EMGname = self.sigs_labels[0,signal][0].encode('utf8')
+            EMGname = 'KIN' + str(signal)
         return EMGname
         # EMG/Kinematic column to decode (FCR,FCU,ECR etc.)
 
@@ -44,46 +52,41 @@ class Rat_decoder(object):
         folder = self.data_dir
         self.data = io.loadmat(folder + file_name)
 
+        # Make directories to save files
+        if not (os.path.isdir(self.save_dir)):
+            os.mkdir(self.save_dir)
+
+        self.animal_dir_save = (self.save_dir + file_name[:-4] + '/') # -4 to get rid of .mat extension
+        if not (os.path.isdir(self.animal_dir_save)):
+            os.mkdir(self.animal_dir_save)
+
+
     def define_decoding_data(self, neural_sig, decoding_sig, decoding_labels):
         self.decoding_sig = decoding_sig    # Name of decoding Sig ('EMG/KIN')
-        self.sigs_labels = self.data[decoding_labels]   # Labels
         self.neural_dat = self.data[neural_sig]
         self.dec_sig_dat = self.data[decoding_sig][:]
+        if decoding_labels:
+            self.sigs_labels = self.data[decoding_labels]   # Labels
 
-    def data_RNN_extraction(self, bins_before, signal):
+    def data_RNN_extraction(self, sig):
         self.num_neurons = self.neural_dat.shape[1]
-        self.num_ex = self.neural_dat.shape[0] - bins_before  # Predictions start after "bins_before" bins
-        self.EMGname = self.get_EMG_name(signal)
+        self.num_ex = self.neural_dat.shape[0] - self.bins_before  # Predictions start after "bins_before" bins
+        self.EMGname = self.get_EMG_name(sig)
 
         # Prepare data to throw into RNN
-        self.y = self.dec_sig_dat[bins_before:bins_before + self.num_ex, signal]
+        self.y = self.dec_sig_dat[self.bins_before:self.bins_before + self.num_ex, sig]
 
         # Make the covariate matrix for an RNN
         # Each example (1st dimension) has "bins_before" bins of neural activity
         # (2nd dimension) for all neurons (3rd dimension)
-        self.X = np.empty((self.num_ex, bins_before, self.num_neurons))
+        self.X = np.empty((self.num_ex, self.bins_before, self.num_neurons))
         self.X[:] = np.nan
         for i in xrange (self.num_ex):
-            self.X[i,:,:] = self.neural_dat[i:i+bins_before,:]
+            self.X[i,:,:] = self.neural_dat[i:i+self.bins_before,:]
 
-        print('EMG: ' + self.EMGname)
-
-        ## Convert data
-
-        # y = y[:,muscle]
-        # io.savemat(folder + 'TestYY.mat', {'y':y})
-
-        plt.figure()
-        plt.plot(self.y)#, label='Validation Set, (Classification Accuracy) = %.2f%s' %(np.max(etr), '%'))
-        plt.title(self.decoding_sig + ': ' + self.EMGname)
-        plt.xlabel('Bins')
-        #plt.ylabel('Percent Correct Classification')
-        # plt.legend(loc='lower right')
-        plt.show(block=False)
-
+        print('Decoding: ' + self.EMGname)
 
     def z_score_data(self):
-
         # PREPROCESS DATA
         #Normalize the inputs
         self.X = stats.zscore(self.X,axis=0)
@@ -99,74 +102,145 @@ class Rat_decoder(object):
         #Make a vector of times, for plotting later
         self.time = np.true_divide(np.arange(self.y.shape[0] + 1),20)
 
-        # Understand Data
-        self.X.shape[0]
+
+    def assign_test_val_sets(self, test_prop, valid_prop, fold):
+
+        test_size = np.int(np.floor(test_prop * self.X.shape[0])) #Floor it to make sure we don't exceed dims of matrix
+        test_begin = np.int( test_size * (fold+1) - test_size ) #If fold = 0, we start from the beginning.
+
+        self.X_test = self.X[test_begin:test_begin + test_size,:,:]
+        self.y_test = self.y[test_begin:test_begin + test_size]
+
+        self.X_train = np.concatenate((self.X[:test_begin,:,:], self.X[test_begin+test_size:,:,:])) #Slice X_test from middle of data
+        self.y_train = np.concatenate((self.y[:test_begin], self.y[test_begin+test_size:]))
+
+        self.time_train = self.time[:self.X_train.shape[0]]
+        # self.time_test = self.time[self.X_train.shape[0]+1:]
 
 
-    def assign_test_val_sets(self, train_prop, test_prop, valid_prop):
-        train_size = np.int(np.round(train_prop * self.X.shape[0]))
-        test_size = np.int(np.round(test_prop * self.X.shape[0]))
+        # train_size = np.int(np.round(train_prop * self.X.shape[0]))
+        # test_size = np.int(np.round(test_prop * self.X.shape[0]))
 
-        self.X_train = self.X[:train_size - self.X.shape[1],:,:] #Subtract X.shape[1] so we don't have overlap in the train/test sets
-        self.y_train = self.y[:train_size - self.X.shape[1]]
+        # self.X_train = self.X[:train_size - self.X.shape[1],:,:] #Subtract X.shape[1] so we don't have overlap in the train/test sets
+        # self.y_train = self.y[:train_size - self.X.shape[1]]
 
-        self.X_test = self.X[train_size:test_size + train_size,:,:]
-        self.y_test = self.y[train_size:test_size + train_size]
+        # self.X_test = self.X[train_size:test_size + train_size,:,:]
+        # self.y_test = self.y[train_size:test_size + train_size]
 
-        self.X_valid = self.X[test_size + train_size:,:,:]
-        self.y_valid = self.y[test_size + train_size:]
+        # self.time_train = self.time[:train_size]
+        # self.time_test = self.time[train_size + 1:]
 
-        if valid_prop !=0:
-            X_trueTest = X_test
-            y_trueTest = y_test
-            X_test = X_valid
-            y_test = y_valid
+        # # Get validation data out of X first if we want validation.
+        # self.X_valid = self.X[test_size + train_size:,:,:]
+        # self.y_valid = self.y[test_size + train_size:]
 
-        self.time_train = self.time[:train_size]
-        self.time_test = self.time[train_size + 1:]
-
-
-    def fit_model(self, units, dropout, num_epochs, verbose_flag, mod=LSTM):
-        #Create model
-        model = Sequential()
-        #If you want to run an LSTM or GRU rather than a simpleRNN, in the next line change "SimpleRNN" to "GRU" or "LSTM"
-        model.add(mod(units,input_shape=(self.X.shape[1],self.X.shape[2]),dropout_W=dropout,dropout_U=dropout))
-        if dropout!=0:
-            model.add(Dropout(dropout))
-        model.add(Dense(1,init='uniform'))
-
-        #Compile model (includes object function and optimization technique)
-        model.compile(loss='mse',optimizer='rmsprop',metrics=['accuracy'])
-
-        #Fit model
-        model.fit(self.X_train,self.y_train,nb_epoch=num_epochs,verbose=verbose_flag)
-
-        #Get predictions on test set
-        self.y_test_pred = model.predict(self.X_test)
-        #Get VAF on test set
-        r2 = self.get_corr(y_test=self.y_test, y_test_pred=self.y_test_pred)
-        print("vaf=", r2)
-
-        #
-        #
-        # #Only if there is validation
         # if valid_prop !=0:
-        #     y_trueTest_pred = model.predict(X_trueTest)
-        #     #Get VAF on test set
-        #     r1 = get_corr(y_test=y_trueTest, y_test_pred=y_trueTest_pred)
+        #     X_trueTest = X_test
+        #     y_trueTest = y_test
+        #     X_test = X_valid
+        #     y_test = y_valid
 
-    def plot_results(self):
+
+    def train_model(self, test_prop, valid_prop, signal=0, allSignals=True, do_folds=False, verbose_flag=0, mod=LSTM):
+
+        if (allSignals == True):
+            signal = np.array([i for i in xrange(self.dec_sig_dat.shape[1])])
+        else: signal = [signal]
+
+        #Loop trhough all signals of interest
+        for sig in signal:
+
+            self.data_RNN_extraction(sig) # Get matrices X, y to train RNN
+            self.z_score_data()           # Normalize data
+
+            num_folds = np.int(np.round(1.0 / test_prop)) # These are the #folds we could split the data into with this test_proportion
+            if (do_folds == True):
+                test_prop = 1.0 / num_folds # Round test_prop according to num_folds
+                folds = [f for f in xrange(num_folds)]
+            else: folds = [num_folds - 1] # If we don't wanna do_folds, just take the last part of the data as testing and do 1 fold.
+
+            #Variables to save / plot
+            self.test_actual = np.array([])
+            self.test_predicted = np.array([])
+            self.vaf = np.array([])
+
+            for fold in folds:
+
+                self.assign_test_val_sets(test_prop, valid_prop, fold) #We only need the test_prop, the train data will be everything else
+
+                #Create model
+                model = Sequential()
+                #If you want to run an LSTM or GRU rather than a simpleRNN, in the next line change "SimpleRNN" to "GRU" or "LSTM"
+                model.add(mod(self.units,input_shape=(self.X.shape[1],self.X.shape[2]),dropout_W=self.dropout,dropout_U=self.dropout))
+                if self.dropout!=0:
+                    model.add(Dropout(self.dropout))
+                model.add(Dense(1,init='uniform'))
+
+                #Compile model (includes object function and optimization technique)
+                model.compile(loss='mse',optimizer='rmsprop',metrics=['accuracy'])
+
+                #Fit model
+                model.fit(self.X_train,self.y_train,nb_epoch=self.num_epochs,verbose=verbose_flag)
+
+                #Get predictions on test set
+                self.y_test_pred = model.predict(self.X_test)
+                #Get VAF on test set
+                vaf = self.get_corr(y_test=self.y_test, y_test_pred=self.y_test_pred)
+
+                print("Decoding {}".format(self.EMGname))
+                print("Fold {} / {}: vaf = {}".format(fold+1,len(folds),vaf))
+
+                #Variables to save / plot
+                self.test_actual = np.append(self.test_actual, self.y_test, axis = 0)
+                self.test_predicted = np.append(self.test_predicted, self.y_test_pred.squeeze(1), axis = 0)
+                self.vaf = np.append(self.vaf, vaf)
+                print (self.vaf)
+
+
+            #######################################
+            # Save results:
+            signal_dir = (self.animal_dir_save + self.EMGname + '/')
+            if not (os.path.isdir(signal_dir)):
+                os.mkdir(signal_dir)
+
+            io.savemat(signal_dir + 'predictions.mat', {'y_actual':(self.test_actual*self.y_std)+self.y_mean,
+                       'y_predicted':(self.test_predicted*self.y_std)+self.y_mean, 'mean_vaf':np.mean(self.vaf), 'VAFs':self.vaf})
+            # io.savemat(signal_dir + 'folds_VAFs.mat', {'VAFs':self.vaf})
+
+            self.plot_results(plot_range=[10], show_flag=False) #Plot and save 10s
+            plt.savefig(signal_dir + 'prediction_fig.png')
+            plt.close('all')
+
+
+    def plot_results(self, plot_range=[], show_flag=False):
         # PLOT FIT
         #Rescale data to its original coordinates
-        y_test_rescale = (self.y_test * self.y_std) + self.y_mean
-        y_test_pred_rescale = (self.y_test_pred * self.y_std) + self.y_mean
+        y_test_rescale = (self.test_actual * self.y_std) + self.y_mean
+        y_test_pred_rescale = (self.test_predicted * self.y_std) + self.y_mean
+        time = np.true_divide(np.arange(self.test_actual.shape[0]), 20)
+
+        if (len(plot_range)==0):    #If no range in seconds is specfied, plot the whole signal
+            plot_range = [time[-1]]
 
         #Plot
-        plt.plot(self.time_test, y_test_rescale)
-        plt.plot(self.time_test, y_test_pred_rescale)
-        # plt.title(sMuscle)
+        plt.figure()
+        plt.plot(time, y_test_rescale, 'blue')
+        plt.plot(time, y_test_pred_rescale, 'orange')
+        plt.title((self.EMGname + ': vaf = {}'.format(np.mean(self.vaf))))
         plt.xlabel('Time (s)')
-        plt.ylabel('EMG')
-        plt.legend(['Actual','Predicted'],bbox_to_anchor=(1, .25))
-        plt.show(block=False)
+        plt.xlim(xmin=0, xmax=plot_range[0])
+        plt.ylabel(self.decoding_sig)
+        plt.legend(['Actual','Predicted'], loc='upper right')
+        if (show_flag):
+            plt.show(block=False)
         #plt.ylim([0, 220])
+
+
+        # # PLOT RAW EMG
+        # plt.figure()
+        # plt.plot(self.y)#, label='Validation Set, (Classification Accuracy) = %.2f%s' %(np.max(etr), '%'))
+        # plt.title(self.decoding_sig + ': ' + self.EMGname)
+        # plt.xlabel('Bins')
+        # #plt.ylabel('Percent Correct Classification')
+        # # plt.legend(loc='lower right')
+        # plt.show(block=False)
--- run_main.py
+++ run_main.py
@@ -5,39 +5,48 @@ from RNN_decoder_class import *
 
 ############################################################
 # IMPORT DATA
-folder = 'data/' #Specify path to DATA FOLDER
+folder = 'data/'       #Specify path to folder containing DATA
 file_name = 'N5_170929_No Obstacles_s_matrices.mat'
-# data = io.loadmat(folder + 'N5_170929_No Obstacles_s_matrices.mat')
 
 ############################################################
-## Variables:
+## DATA variables:
 bins_before = 10
-neural_sig = 'APdat'        # Name of neural data
-decoding_sig = 'EMGdat'     # Usually: 'EMGdat' / 'KINdat' (!!string)
-decoding_labels = 'EMGlabels'  # Usually: 'EMGlabels' / 'KINlabels' (!!string)
-signal = 1                  # EMG/Kinematic column to decode (FCR,FCU,ECR etc.)
+neural_sig = 'APdat'            # Name of neural data
+decoding_sig = 'EMGdat'         # Usually: 'EMGdat' / 'KINdat' (!!string)
+decoding_labels = 'EMGlabels'   # Usually: 'EMGlabels' / 'KINlabels' (!!string) -> Leave as Empty string otherwise
+signal = 1                      # EMG/Kinematic column to decode (FCR,FCU,ECR etc.)
+allSignals = False               # If you want to analyze all signals (all EMGs/KINs) set this to TRUE. It won't matter what the variable 'signal' is then.
 
+## NNet variables:
 units = 50
 dropout = 0
 num_epochs = 10
-verbose_flag = 1 #If you want to see the output during training
+verbose_flag = 1 #If you want to print the output during training
 
-train_prop = 0.90
-test_prop = 0.10
-valid_prop = 0 #If you are seeing what parameters work best, you should do this on a separate validation set, to avoid overfitting to the test set
+test_prop = 0.10 # The train_prop will automatically be 1-test_prop
+valid_prop = 0  # Validation is used to test performance over training to avoid overfitting. Not implement yet, leave as 0.
+
+do_folds = True
+# If DO_FOLDS is True, the analysis will be done over the whole data length in
+# n folds according to the train/test props: E.g. -> train = 0.9, test = 0.1 -> 10 folds
+# If DO_FOLDS is False, the RNN will be trained on first 'train_prop' data, and tested on last 'test_prop' amount of data.
 
 ############################################################
 # RUN RNN
 ############################################################
-rnn_dec = Rat_decoder(folder)
+rnn = Rat_decoder(folder, bins_before, units, dropout, num_epochs)
 
 
-rnn_dec.import_data(file_name)
-rnn_dec.define_decoding_data(neural_sig, decoding_sig, decoding_labels)
-rnn_dec.data_RNN_extraction(bins_before, signal)
-rnn_dec.z_score_data()
-rnn_dec.assign_test_val_sets(train_prop, test_prop, valid_prop)
+rnn.import_data(file_name)
 
-rnn_dec.fit_model(units, dropout, num_epochs, verbose_flag, mod=LSTM)
+rnn.define_decoding_data(neural_sig, decoding_sig, decoding_labels)
 
-rnn_dec.plot_results()
+# rnn.data_RNN_extraction(bins_before, signal)
+#
+# rnn.z_score_data()
+#
+# rnn.assign_test_val_sets(train_prop, test_prop, valid_prop)
+
+rnn.train_model(test_prop, valid_prop, signal=signal, allSignals=allSignals, folds=False, verbose_flag=verbose_flag , mod=LSTM)
+
+rnn.plot_results()
